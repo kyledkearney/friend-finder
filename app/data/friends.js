5d2397e3bd159ae93dc4c13ff4f6099f65c41067
hyper linked <45010713+kyledkearney@users.noreply.github.com>
2019-03-25 22:46:04 -0600

    final upload

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+		name: 'Jay Leno',
+		photo: 'https://media13.s-nbcnews.com/j/MSNBC/Components/Video/201903/tdy_pop_9a_jay_leno_190312_1920x1080.social_share_1024x768_scale.jpg',
+		scores: [3, 5, 4, 4, 5, 5, 5, 4, 5, 5]
+	},{
+		name: 'Jerry Seinfeld',
+		photo: 'http://data.logograph.com/resize/KingCenter/multimedia/Image/6044/JerrySeinfeld900x600.jpg?width=1500',
+		scores: [4, 4, 2, 5, 5, 4, 5, 4, 1, 3]
+	},{
+		name: 'Patrick Dempsy',
+		photo: 'http://cdn.kdhamptons.com/wp-content/uploads/2014/10/17051539/Sacoor-Lodge_1734B_flat.jpg',
+		scores: [3, 2, 1, 5, 5, 5, 4, 2, 5, 4]
+	},{
+		name: 'Tim Allen',
+		photo: 'https://m.media-amazon.com/images/M/MV5BMTI5ODY0NTAwOF5BMl5BanBnXkFtZTcwOTI3NjQxMw@@._V1_UX214_CR0,0,214,317_AL_.jpg',
+		scores: [1, 5, 5, 2, 5, 1, 5, 5, 3, 5]
+	}];
+
+module.exports = friends;
